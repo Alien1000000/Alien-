03df602334430c8d464a0428edc175de2efc2ab3
Alien1000000 <Avimoas1@gmail.com>
2026-09-01 17:45:33 +0300

    Update android/app/build.gradle.kts for app language switch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.alienavi.aliennews"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.0.3"
+        versionCode = 11
+        versionName = "2.0.4"
 
         vectorDrawables.useSupportLibrary = true
     }
